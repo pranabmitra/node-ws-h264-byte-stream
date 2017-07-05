5e6e1f704b1b4b45823fab700b24225da8300e77
pranab <pranab@ipvisionsoft.com>
2017-07-05 17:10:43 +0600

    send h264 byte stream via websocket

---
+++ ws_server.js
@@ -0,0 +1,50 @@
+const WebSocket = require('ws');
+const fs = require('fs');
+const wss = new WebSocket.Server({ port: 9001 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        // received message
+    });
+
+    ws.send('Start Counting...');
+
+    var counter = 1;
+    var timer = setInterval(function onSetInterval() {
+        sendByteStream();
+    }, 1500);
+
+    function sendByteStream() {
+        var file = './test/sample_video/' + (counter++)  + '.h264';
+
+        if (fs.existsSync(file)) {
+            var stat = fs.statSync(file);
+
+            // ws.send(fs.createReadStream(file, {start: 0, end: stat.size}));
+
+            // var readStream = fs.createReadStream(file,
+            // {
+            // start: 0,
+            // end: stat.size
+            // });
+            // readStream.on('data', function(data) {
+            // console.log(counter);
+            // ws.send(data);
+
+            // });
+
+            fs.readFile(file, function(err,data) {
+                if(err){console.log(err)}
+                // ws.send(data,{binary:true});
+                console.log(counter, data.buffer);
+                ws.send((data));
+
+            });
+        } else {
+            ws.send('No more files!');
+            clearInterval(timer);
+        }
+
+    }
+
+});
